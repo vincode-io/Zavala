bce50e63d69cf2331cd87c1ab2f9e694ca9d9670
Maurice Parker <mo@vincode.io>
2020-11-10 04:26:44 -0600

    Added MainSplitViewController

---
+++ Manhattan/MainSplitViewController.swift
@@ -0,0 +1,18 @@
+//
+//  MainSplitViewController.swift
+//  Manhattan
+//
+//  Created by Maurice Parker on 11/10/20.
+//
+
+import UIKit
+
+class MainSplitViewController: UISplitViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
