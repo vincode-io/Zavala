1a1fa32c33c9d01a64304813058d70c4f199ee8e
Maurice Parker <mo@vincode.io>
2020-11-16 19:56:35 -0600

    Fix misspelled variable

--- Manhattan/Editor/EditorCollectionViewCell.swift
+++ Manhattan/Editor/EditorCollectionViewCell.swift
@@ -22,7 +22,7 @@ class EditorCollectionViewCell: UICollectionViewListCell {
 		var content = EditorContentConfiguration().updated(for: state)
 		content.editableText = editableText
 		if traitCollection.userInterfaceIdiom == .mac && accessories.isEmpty {
-			content.indentionWidth = indentationWidth
+			content.indentationWidth = indentationWidth
 		}
 		contentConfiguration = content
 	}
@@ -32,7 +32,7 @@ class EditorCollectionViewCell: UICollectionViewListCell {
 struct EditorContentConfiguration: UIContentConfiguration, Hashable {
 
 	var editableText: String? = nil
-	var indentionWidth: CGFloat? = nil
+	var indentationWidth: CGFloat? = nil
 	
 	func makeContentView() -> UIView & UIContentView {
 		return EditorContentView(configuration: self)
@@ -83,7 +83,7 @@ class EditorContentView: UIView, UIContentView {
 
 		textView.removeConstraintsAssociatedWithSuperView()
 		NSLayoutConstraint.activate([
-			textView.leadingAnchor.constraint(equalTo: layoutMarginsGuide.leadingAnchor, constant: configuration.indentionWidth ?? 0.0),
+			textView.leadingAnchor.constraint(equalTo: layoutMarginsGuide.leadingAnchor, constant: configuration.indentationWidth ?? 0.0),
 			textView.trailingAnchor.constraint(equalTo: layoutMarginsGuide.trailingAnchor),
 			textView.topAnchor.constraint(equalTo: layoutMarginsGuide.topAnchor),
 			textView.bottomAnchor.constraint(equalTo: layoutMarginsGuide.bottomAnchor)
