9c7c5cabd553b69dc79d71d3e0a8ae2392cdeb66
Maurice Parker <mo@vincode.io>
2020-11-24 06:55:12 -0600

    Remove unnecessary function

--- Manhattan/Editor/EditorViewController.swift
+++ Manhattan/Editor/EditorViewController.swift
@@ -90,10 +90,6 @@ extension EditorViewController {
 		return layout
 	}
 	
-	override func numberOfSections(in collectionView: UICollectionView) -> Int {
-		return 	1
-	}
-	
 	override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
 		return 10
 	}
