7c67fc48ea49e254184ab26e0e8da0c013d55135
Maurice Parker <mo@vincode.io>
2020-11-23 20:30:21 -0600

    Removed all diffable datasource update stuff

--- Manhattan/Editor/EditorViewController.swift
+++ Manhattan/Editor/EditorViewController.swift
@@ -31,8 +31,9 @@ class EditorViewController: UICollectionViewController {
 				
 				guard isViewLoaded else { return }
 				updateUI()
-				applySnapshot(animated: false)
-				moveCursorToInitialPosition()
+				collectionView.reloadData()
+//				applySnapshot(animated: false)
+//				moveCursorToInitialPosition()
 			}
 		}
 		
@@ -40,8 +41,7 @@ class EditorViewController: UICollectionViewController {
 	
 	private var favoriteBarButtonItem: UIBarButtonItem?
 	
-	private let dataSourceQueue = MainThreadOperationQueue()
-	private var dataSource: UICollectionViewDiffableDataSource<Int, EditorItem>!
+	private var editorRegistration: UICollectionView.CellRegistration<EditorCollectionViewCell, EditorItem>?
 	
 	override func viewDidLoad() {
         super.viewDidLoad()
@@ -55,11 +55,19 @@ class EditorViewController: UICollectionViewController {
 		
 		collectionView.allowsSelection = false
 		collectionView.collectionViewLayout = createLayout()
-		configureDataSource()
+		collectionView.dataSource = self
+
+		editorRegistration = UICollectionView.CellRegistration<EditorCollectionViewCell, EditorItem> { (cell, indexPath, editorItem) in
+			cell.editorItem = editorItem
+			cell.delegate = self
+		}
+		
+//		configureDataSource()
 
 		updateUI()
-		applySnapshot(animated: false)
-		moveCursorToInitialPosition()
+		collectionView.reloadData()
+//		applySnapshot(animated: false)
+//		moveCursorToInitialPosition()
 	}
 
 	// MARK: Actions
@@ -82,82 +90,38 @@ extension EditorViewController {
 		return layout
 	}
 	
-	private func configureDataSource() {
-		let editorRegistration = UICollectionView.CellRegistration<EditorCollectionViewCell, EditorItem> { (cell, indexPath, item) in
-			cell.editorItem = item
-			cell.delegate = self
-		}
-
-		dataSource = UICollectionViewDiffableDataSource<Int, EditorItem>(collectionView: collectionView) { (collectionView, indexPath, item) -> UICollectionViewCell in
-			return collectionView.dequeueConfiguredReusableCell(using: editorRegistration, for: indexPath, item: item)
-		}
-		
-		dataSource.sectionSnapshotHandlers.willExpandItem = { [weak self] item in
-			self?.outline?.expandHeadline(headlineID: item.id)
-		}
-		
-		dataSource.sectionSnapshotHandlers.willCollapseItem = { [weak self] item in
-			self?.outline?.collapseHeadline(headlineID: item.id)
-		}
-		
+	override func numberOfSections(in collectionView: UICollectionView) -> Int {
+		return 	1
 	}
 	
-	private func delete(items: [EditorItem], animated: Bool) {
-		dataSourceQueue.add(DeleteItemsOperation(dataSource: dataSource, section: 0, items: items, animated: animated))
-	}
-
-	private func insert(items: [EditorItem], afterItem: EditorItem, animated: Bool) {
-		dataSourceQueue.add(InsertItemsOperation(dataSource: dataSource, collectionView: collectionView, section: 0, items: items, afterItem: afterItem, animated: animated))
-	}
-
-	private func indent(items: [EditorItem], newParentItem: EditorItem, animated: Bool) {
-		dataSourceQueue.add(IndentItemsOperation(dataSource: dataSource, collectionView: collectionView, section: 0, items: items, newParentItem: newParentItem, animated: animated))
-	}
-
-	private func reload(items: [EditorItem], animated: Bool) {
-		dataSourceQueue.add(ReloadItemsOperation(dataSource: dataSource, collectionView: collectionView, section: 0, items: items, animated: animated))
-	}
-
-	private func moveCursor(item: EditorItem, direction: MoveCursorOperation.Direction) {
-		dataSourceQueue.add(MoveCursorOperation(dataSource: dataSource, collectionView: collectionView, item: item, direction: direction))
+	override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+		return 10
 	}
 	
-	private func applySnapshot(animated: Bool) {
-		var snapshot = NSDiffableDataSourceSectionSnapshot<EditorItem>()
-		
-		if let items = outline?.headlines?.map({ EditorItem.editorItem($0) }) {
-			snapshot.append(items)
-			applySnapshot(&snapshot, items: items)
-		}
-		
-		dataSourceQueue.add(ApplySnapshotOperation(dataSource: dataSource, section: 0, snapshot: snapshot, animated: animated))
+	override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+		let editorItem = EditorItem.editorItem(Headline(plainText: "This is a test..."))
+		return collectionView.dequeueConfiguredReusableCell(using: editorRegistration!, for: indexPath, item: editorItem)
 	}
 	
-	private func applySnapshot( _ snapshot: inout NSDiffableDataSourceSectionSnapshot<EditorItem>, items: [EditorItem]) {
-		expandAndCollapse(snapshot: &snapshot, items: items)
-		for item in items {
-			snapshot.append(item.children, to: item)
-			if !item.children.isEmpty {
-				applySnapshot(&snapshot, items: item.children)
-			}
-		}
-	}
-	
-	private func expandAndCollapse(snapshot: inout NSDiffableDataSourceSectionSnapshot<EditorItem>, items: [EditorItem]) {
-		var expandItems = [EditorItem]()
-		var collapseItems = [EditorItem]()
-		
-		for item in items {
-			if item.isExpanded {
-				expandItems.append(item)
-			} else {
-				collapseItems.append(item)
-			}
-		}
-		
-		snapshot.expand(expandItems)
-		snapshot.collapse(collapseItems)
-	}
+//	private func configureDataSource() {
+//		let editorRegistration = UICollectionView.CellRegistration<EditorCollectionViewCell, EditorItem> { (cell, indexPath, item) in
+//			cell.editorItem = item
+//			cell.delegate = self
+//		}
+//
+//		dataSource = UICollectionViewDiffableDataSource<Int, EditorItem>(collectionView: collectionView) { (collectionView, indexPath, item) -> UICollectionViewCell in
+//			return collectionView.dequeueConfiguredReusableCell(using: editorRegistration, for: indexPath, item: item)
+//		}
+//
+//		dataSource.sectionSnapshotHandlers.willExpandItem = { [weak self] item in
+//			self?.outline?.expandHeadline(headlineID: item.id)
+//		}
+//
+//		dataSource.sectionSnapshotHandlers.willCollapseItem = { [weak self] item in
+//			self?.outline?.collapseHeadline(headlineID: item.id)
+//		}
+//
+//	}
 	
 }
 
@@ -166,35 +130,22 @@ extension EditorViewController: EditorCollectionViewCellDelegate {
 	func textChanged(item: EditorItem, attributedText: NSAttributedString) {
 		if item.attributedText != attributedText {
 			outline?.updateHeadline(headlineID: item.id, attributedText: attributedText)
-			item.attributedText = attributedText
-			self.reload(items: [item], animated: false)
 		}
 	}
 	
 	func deleteHeadline(item: EditorItem) {
 		outline?.deleteHeadline(headlineID: item.id)
-		moveCursor(item: item, direction: .up)
-		delete(items: [item], animated: true)
 	}
 	
 	// TODO: Need to take into consideration expanded state when placing the new Headline
 	func createHeadline(item: EditorItem) {
-		guard let headline = outline?.createHeadline(afterHeadlineID: item.id) else { return }
-		let newItem = EditorItem.editorItem(headline)
-		insert(items: [newItem], afterItem: item, animated: false)
+//		guard let headline = outline?.createHeadline(afterHeadlineID: item.id) else { return }
 	}
 	
 	func indent(item: EditorItem, attributedText: NSAttributedString) {
 		outline?.updateHeadline(headlineID: item.id, attributedText: attributedText)
-//		print("================================")
-//		print(outline!)
 		guard let (headline, newParentHeadline) = outline?.indentHeadline(headlineID: item.id) else { return }
-//		print(outline!)
-//		print("================================")
 
-		let headlineItem = EditorItem.editorItem(headline)
-		let newParentItem = EditorItem.editorItem(newParentHeadline)
-		indent(items: [headlineItem], newParentItem: newParentItem, animated: true)
 	}
 	
 	func outdent(item: EditorItem, attributedText: NSAttributedString) {
@@ -202,11 +153,9 @@ extension EditorViewController: EditorCollectionViewCellDelegate {
 	}
 	
 	func moveUp(item: EditorItem) {
-		moveCursor(item: item, direction: .up)
 	}
 	
 	func moveDown(item: EditorItem) {
-		moveCursor(item: item, direction: .down)
 	}
 	
 }
@@ -214,12 +163,6 @@ extension EditorViewController: EditorCollectionViewCellDelegate {
 // MARK: Helpers
 
 private extension EditorViewController {
-	
-	private func moveCursorToInitialPosition() {
-		guard let headline = outline?.headlines?.first else { return }
-		let item = EditorItem.editorItem(headline)
-		moveCursor(item: item, direction: .none)
-	}
 	
 	private func updateUI() {
 		navigationItem.title = outline?.title
--- Manhattan/Utility/ApplySnapshotOperation.swift
+++ Manhattan/Utility/ApplySnapshotOperation.swift
@@ -11,7 +11,6 @@ import RSCore
 class ApplySnapshotOperation<S: Hashable, I: Hashable>: MainThreadOperationBase {
 	
 	private var dataSource: UICollectionViewDiffableDataSource<S, I>
-	private var collectionView: UICollectionView?
 	private var section: S
 	private var snapshot: NSDiffableDataSourceSectionSnapshot<I>
 	private var animated: Bool
@@ -21,13 +20,11 @@ class ApplySnapshotOperation<S: Hashable, I: Hashable>: MainThreadOperationBase 
 	is typically what we want.
 	*/
 	init(dataSource: UICollectionViewDiffableDataSource<S, I>,
-		 collectionView: UICollectionView? = nil,
 		 section: S,
 		 snapshot: NSDiffableDataSourceSectionSnapshot<I>,
 		 animated: Bool) {
 		
 		self.dataSource = dataSource
-		self.collectionView = collectionView
 		self.section = section
 		self.snapshot = snapshot
 		self.animated = animated
@@ -35,21 +32,8 @@ class ApplySnapshotOperation<S: Hashable, I: Hashable>: MainThreadOperationBase 
 	}
 	
 	override func run() {		
-		let textCursorSource = UIResponder.currentFirstResponder as? TextCursorSource
-		let item = textCursorSource?.identifier as? I
-		let selectedRange = textCursorSource?.selectedTextRange
-		
 		dataSource.apply(snapshot, to: section, animatingDifferences: animated) { [weak self] in
 			guard let self = self else { return }
-
-			if let item = item, let indexPath = self.dataSource.indexPath(for: item), let textCursor = self.collectionView?.cellForItem(at: indexPath) as? TextCursorTarget {
-				if let selectedRange = selectedRange {
-					textCursor.restoreSelection(selectedRange)
-				} else {
-					textCursor.moveToEnd()
-				}
-			}
-
 			self.operationDelegate?.operationDidComplete(self)
 		}
 	}
