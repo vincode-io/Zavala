9488aaa9d1506c5137187d5b3070ae08e91869b8
Maurice Parker <mo@vincode.io>
2020-12-07 00:38:35 -0600

    Remove the concept of IndexPaths from Templeton as Templeton should know nothing about sections

---
+++ Manhattan/Utility/ShadowTableChanges+.swift
@@ -0,0 +1,33 @@
+//
+//  ShadowTableChanges+.swift
+//  Manhattan
+//
+//  Created by Maurice Parker on 12/7/20.
+//
+
+import Foundation
+import Templeton
+
+extension ShadowTableChanges {
+	
+	public var deleteIndexPaths: [IndexPath]? {
+		guard let deletes = deletes else { return nil }
+		return deletes.map { IndexPath(row: $0, section: 0) }
+	}
+	
+	public var insertIndexPaths: [IndexPath]? {
+		guard let inserts = inserts else { return nil }
+		return inserts.map { IndexPath(row: $0, section: 0) }
+	}
+	
+	public var moveIndexPaths: [(IndexPath, IndexPath)]? {
+		guard let moves = moves else { return nil }
+		return moves.map { (IndexPath(row: $0.from, section: 0), IndexPath(row: $0.to, section: 0)) }
+	}
+	
+	public var reloadIndexPaths: [IndexPath]? {
+		guard let reloads = reloads else { return nil }
+		return reloads.map { IndexPath(row: $0, section: 0) }
+	}
+	
+}
--- Templeton/Sources/Templeton/ShadowTableChanges.swift
+++ Templeton/Sources/Templeton/ShadowTableChanges.swift
@@ -10,8 +10,8 @@ import Foundation
 public struct ShadowTableChanges {
 	
 	public struct Move: Hashable {
-		var from: Int
-		var to: Int
+		public var from: Int
+		public var to: Int
 		
 		init(_ from: Int, _ to: Int) {
 			self.from = from
@@ -26,26 +26,6 @@ public struct ShadowTableChanges {
 	
 	public var isEmpty: Bool {
 		return deletes == nil && inserts == nil && reloads == nil
-	}
-	
-	public var deleteIndexPaths: [IndexPath]? {
-		guard let deletes = deletes else { return nil }
-		return deletes.map { IndexPath(row: $0, section: 0) }
-	}
-	
-	public var insertIndexPaths: [IndexPath]? {
-		guard let inserts = inserts else { return nil }
-		return inserts.map { IndexPath(row: $0, section: 0) }
-	}
-	
-	public var moveIndexPaths: [(IndexPath, IndexPath)]? {
-		guard let moves = moves else { return nil }
-		return moves.map { (IndexPath(row: $0.from, section: 0), IndexPath(row: $0.to, section: 0)) }
-	}
-	
-	public var reloadIndexPaths: [IndexPath]? {
-		guard let reloads = reloads else { return nil }
-		return reloads.map { IndexPath(row: $0, section: 0) }
 	}
 	
 	init(deletes: Set<Int>? = nil, inserts: Set<Int>? = nil, moves: Set<Move>? = nil, reloads: Set<Int>? = nil) {
