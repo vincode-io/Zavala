9fd1d61e62c3df9937b7289008bbd4aa7c8c7b7b
Maurice Parker <mo@vincode.io>
2020-12-26 01:04:36 -0600

    Fix compile error

--- Zavala/AppDelegate.swift
+++ Zavala/AppDelegate.swift
@@ -55,7 +55,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 		}
 		
 		if !(mainSplitViewController?.isToggleRowCompleteUnavailable ?? true) {
-			if mainSplitViewController?.isCurrentrowComplete ?? false {
+			if mainSplitViewController?.isCurrentRowComplete ?? false {
 				menuKeyCommands.append(uncompleteRowCommand)
 			} else {
 				menuKeyCommands.append(completeRowCommand)
