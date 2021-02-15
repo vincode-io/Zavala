5bfd85143a711a6d30bed26b120897e9fb332fce
Maurice Parker <mo@vincode.io>
2021-02-15 14:58:38 -0600

    Add some CloudKit implementation

--- Templeton/Sources/Templeton/CloudKit/CloudKitManager.swift
+++ Templeton/Sources/Templeton/CloudKit/CloudKitManager.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import CloudKit
 
 public class CloudKitManager {
 	
@@ -18,6 +19,30 @@ public class CloudKitManager {
 	struct ActionRequest {
 		let action: Action
 		let id: EntityID
+	}
+
+	private let container: CKContainer = {
+		let orgID = Bundle.main.object(forInfoDictionaryKey: "OrganizationIdentifier") as! String
+		return CKContainer(identifier: "iCloud.\(orgID).Zavala")
+	}()
+
+	private let defaultZone: CloudKitOutlineZone
+
+	init() {
+		defaultZone = CloudKitOutlineZone(container: container)
+	}
+	
+	func addRequest(action: Action, id: EntityID) {
+		let actionRequest = ActionRequest(action: action, id: id)
+		// Now do something with it
+	}
+	
+	func accountDidInitialize(_ account: Account) {
+		defaultZone.delegate = CloudKitAcountZoneDelegate(account: account)
+	}
+	
+	func accountWillBeDeleted(_ account: Account) {
+		defaultZone.resetChangeToken()
 	}
 	
 }
