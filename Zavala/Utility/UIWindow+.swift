92b39001dc12015106f87cb1c7d2b302dc97abbb
Maurice Parker <mo@vincode.io>
2021-03-19 18:35:37 -0500

    Fix crash that could happen if the window didn't implement uiWindow

--- Zavala/Utility/UIWindow+.swift
+++ Zavala/Utility/UIWindow+.swift
@@ -12,9 +12,11 @@ extension UIWindow {
 	var nsWindow: NSObject? {
 		guard let nsWindows = NSClassFromString("NSApplication")?.value(forKeyPath: "sharedApplication.windows") as? [NSObject] else { return nil }
 		for nsWindow in nsWindows {
-			let uiWindows = nsWindow.value(forKeyPath: "uiWindows") as? [UIWindow] ?? []
-			if uiWindows.contains(self) {
-				return nsWindow
+			if nsWindow.responds(to: NSSelectorFromString("uiWindows")) {
+				let uiWindows = nsWindow.value(forKeyPath: "uiWindows") as? [UIWindow] ?? []
+				if uiWindows.contains(self) {
+					return nsWindow
+				}
 			}
 		}
 		return nil
