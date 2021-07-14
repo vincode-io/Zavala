ff22a4eedd94095618f8611950d1311717a06cbc
Maurice Parker <mo@vincode.io>
2021-07-14 18:16:28 -0500

    Change how we resume and suspend to try to avoid deadlocks.

--- SpotlightIndexExtension/IndexRequestHandler.swift
+++ SpotlightIndexExtension/IndexRequestHandler.swift
@@ -11,43 +11,59 @@ import Templeton
 
 class IndexRequestHandler: CSIndexExtensionRequestHandler {
 	
-	override init() {
-		DispatchQueue.main.sync {
-			if AccountManager.shared == nil {
-				let appGroup = Bundle.main.object(forInfoDictionaryKey: "AppGroup") as! String
-				let containerURL = FileManager.default.containerURL(forSecurityApplicationGroupIdentifier: appGroup)
-				let documentAccountsFolderPath = containerURL!.appendingPathComponent("Accounts").path
-				AccountManager.shared = AccountManager(accountsFolderPath: documentAccountsFolderPath)
-			}
-		}
-	}
-
     override func searchableIndex(_ searchableIndex: CSSearchableIndex, reindexAllSearchableItemsWithAcknowledgementHandler acknowledgementHandler: @escaping () -> Void) {
 		DispatchQueue.main.async {
-			var searchableItems = [CSSearchableItem]()
+			self.resume()
 			
+			var searchableItems = [CSSearchableItem]()
 			for document in AccountManager.shared.documents {
 				searchableItems.append(DocumentIndexer.makeSearchableItem(forDocument: document))
 			}
 			
-			searchableIndex.indexSearchableItems(searchableItems)
-			acknowledgementHandler()
+			self.suspend()
+			
+			searchableIndex.indexSearchableItems(searchableItems) { _ in
+				acknowledgementHandler()
+			}
 		}
     }
     
 	override func searchableIndex(_ searchableIndex: CSSearchableIndex, reindexSearchableItemsWithIdentifiers identifiers: [String], acknowledgementHandler: @escaping () -> Void) {
 		DispatchQueue.main.async {
-			var searchableItems = [CSSearchableItem]()
+			self.resume()
 			
+			var searchableItems = [CSSearchableItem]()
 			for description in identifiers {
 				if let entityID = EntityID(description: description), let document = AccountManager.shared.findDocument(entityID) {
 					searchableItems.append(DocumentIndexer.makeSearchableItem(forDocument: document))
 				}
 			}
 			
-			searchableIndex.indexSearchableItems(searchableItems)
-			acknowledgementHandler()
+			self.suspend()
+			
+			searchableIndex.indexSearchableItems(searchableItems) { _ in
+				acknowledgementHandler()
+			}
 		}
+	}
+	
+}
+
+extension IndexRequestHandler {
+	
+	private func resume() {
+		if AccountManager.shared == nil {
+			let appGroup = Bundle.main.object(forInfoDictionaryKey: "AppGroup") as! String
+			let containerURL = FileManager.default.containerURL(forSecurityApplicationGroupIdentifier: appGroup)
+			let documentAccountsFolderPath = containerURL!.appendingPathComponent("Accounts").path
+			AccountManager.shared = AccountManager(accountsFolderPath: documentAccountsFolderPath)
+		} else {
+			AccountManager.shared.resume()
+		}
+	}
+	
+	private func suspend() {
+		AccountManager.shared.suspend()
 	}
 	
 }
