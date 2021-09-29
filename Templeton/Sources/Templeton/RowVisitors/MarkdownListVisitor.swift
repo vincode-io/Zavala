c58eeae85b6ecd75d84eaab43fac411cda1bbc2b
Maurice Parker <mo@vincode.io>
2021-09-29 14:56:09 -0500

    Fixed Markdown List formatting when notes are present

--- Templeton/Sources/Templeton/RowVisitors/MarkdownListVisitor.swift
+++ Templeton/Sources/Templeton/RowVisitors/MarkdownListVisitor.swift
@@ -23,13 +23,15 @@ class MarkdownListVisitor {
 			markdown.append("* \(textRow.topicMarkdown ?? "")")
 		}
 		
-		if let notePlainText = textRow.noteMarkdown {
-			markdown.append("\n  \(notePlainText)")
+		if let noteMarkdown = textRow.noteMarkdown, !noteMarkdown.isEmpty {
+			markdown.append("\n\n")
+			markdown.append(String(repeating: "\t", count: indentLevel))
+			markdown.append("  \(noteMarkdown)")
 		}
 		
 		indentLevel = indentLevel + 1
 		textRow.rows.forEach {
-			markdown.append("\n")
+			markdown.append("\n\n")
 			$0.visit(visitor: self.visitor)
 		}
 		indentLevel = indentLevel - 1
