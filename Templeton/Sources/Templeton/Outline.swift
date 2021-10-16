155545a31930fe1233cb3c8547647ea2b98df95d
Maurice Parker <mo@vincode.io>
2021-10-16 09:53:59 -0500

    Fix a bug where the images wasn't removed from disk when deleting a row with an image in it

--- Templeton/Sources/Templeton/Outline.swift
+++ Templeton/Sources/Templeton/Outline.swift
@@ -797,6 +797,11 @@ public final class Outline: RowContainer, OPMLImporter, Identifiable, Equatable,
 		imagesFile?.markAsDirty()
 	}
 	
+	func removeImages(rowID: String) {
+		self.images?.removeValue(forKey: rowID)
+		imagesFile?.markAsDirty()
+	}
+	
 	func createNotes(rows: [Row], rowStrings: RowStrings?) -> ([Row], Int?) {
 		beginCloudKitBatchRequest()
 		
@@ -895,6 +900,7 @@ public final class Outline: RowContainer, OPMLImporter, Identifiable, Equatable,
 
 		for row in rows {
 			row.parent?.removeRow(row)
+			removeImages(rowID: row.id)
 			guard let rowShadowTableIndex = row.shadowTableIndex else { continue }
 			deletes.insert(rowShadowTableIndex)
 		}
