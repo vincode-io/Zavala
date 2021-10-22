20ea8f65a57b611061fa0d5731bbb284a7cdf6cd
Maurice Parker <mo@vincode.io>
2021-10-22 11:32:38 -0500

    Flesh out the IntentRow

--- Zavala/Intents/IntentRow+.swift
+++ Zavala/Intents/IntentRow+.swift
@@ -13,6 +13,10 @@ extension IntentRow {
 	convenience init(_ row: Row) {
 		self.init(identifier: row.entityID.description, display: row.topicMarkdown ?? "")
 		entityID = IntentEntityID(row.entityID)
+		topic = row.topicMarkdown
+		note = row.noteMarkdown
+		complete = NSNumber(booleanLiteral: row.isComplete)
+		expanded = NSNumber(booleanLiteral: row.isExpanded)
 	}
 	
 }
