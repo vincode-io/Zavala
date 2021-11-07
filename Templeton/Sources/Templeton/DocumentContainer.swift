22bfa3240a5753299f4b4c5134cb297678e7829a
Maurice Parker <mo@vincode.io>
2021-11-07 09:55:40 -0600

    Sort title names

--- Templeton/Sources/Templeton/DocumentContainer.swift
+++ Templeton/Sources/Templeton/DocumentContainer.swift
@@ -36,6 +36,6 @@ public extension Array where Element == DocumentContainer {
     }
 
     var title: String {
-        ListFormatter.localizedString(byJoining: self.compactMap({ $0.name }))
+        ListFormatter.localizedString(byJoining: self.compactMap({ $0.name }).sorted())
     }
 }
