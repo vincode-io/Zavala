22af3b4dde3862353f3d96ed99dce95808382751
Maurice Parker <mo@vincode.io>
2021-11-17 09:49:14 -0600

    Change to using a serial queue instead of a lock to prevent blocking the main thread

--- Templeton/Sources/Templeton/CloudKit/CloudKitManager.swift
+++ Templeton/Sources/Templeton/CloudKit/CloudKitManager.swift
@@ -39,7 +39,9 @@ public class CloudKitManager {
 
 	private weak var errorHandler: ErrorHandler?
 	private weak var account: Account?
-	
+
+	private let requestsFileQueue = DispatchQueue(label: "Requests File Queue")
+	private var writeRequestsBackgroundTaskID = UIBackgroundTaskIdentifier.invalid
 	private var sendChangesBackgroundTaskID = UIBackgroundTaskIdentifier.invalid
 
 	private var coalescingQueue = CoalescingQueue(name: "Send Modifications", interval: 5)
@@ -69,9 +71,7 @@ public class CloudKitManager {
 
 		 return (isReachable && !needsConnection)
 	}
-	
-	private var requestsFileLock = NSLock()
-	
+		
 	init(account: Account, errorHandler: ErrorHandler) {
 		self.account = account
 		self.defaultZone = CloudKitOutlineZone(container: container)
@@ -99,42 +99,21 @@ public class CloudKitManager {
 	}
 	
 	func addRequests(_ requests: Set<CloudKitActionRequest>) {
-		let processInfo = ProcessInfo()
-		processInfo.performExpiringActivity(withReason: "Fetching Changes") { [weak self] expired in
-			guard let self = self else { return }
-			
-			guard !expired else {
-				return
-			}
+		let completeProcessing = { [unowned self] in
+			UIApplication.shared.endBackgroundTask(self.writeRequestsBackgroundTaskID)
+			self.writeRequestsBackgroundTaskID = UIBackgroundTaskIdentifier.invalid
+		}
 
-			self.requestsFileLock.lock()
-			defer {
-				self.requestsFileLock.unlock()
-			}
-			
-			let queuedRequests: Set<CloudKitActionRequest>
-			if let fileData = try? Data(contentsOf: CloudKitActionRequest.actionRequestFile) {
-				let decoder = PropertyListDecoder()
-				if let decodedRequests = try? decoder.decode(Set<CloudKitActionRequest>.self, from: fileData) {
-					queuedRequests = decodedRequests
-				} else {
-					queuedRequests = Set<CloudKitActionRequest>()
-				}
-			} else {
-				queuedRequests = Set<CloudKitActionRequest>()
-			}
-
-			let mergedRequests = queuedRequests.union(requests)
-			
-			let encoder = PropertyListEncoder()
-			encoder.outputFormat = .binary
-
-			if let encodedIDs = try? encoder.encode(mergedRequests) {
-				try? encodedIDs.write(to: CloudKitActionRequest.actionRequestFile)
-			}
-			
+		self.writeRequestsBackgroundTaskID = UIApplication.shared.beginBackgroundTask {
+			completeProcessing()
+			os_log("CloudKit add requests terminated for running too long.", log: self.log, type: .info)
+		}
+		
+		requestsFileQueue.async {
+			self.writerRequests(requests)
 			DispatchQueue.main.async {
 				self.coalescingQueue.add(self, #selector(self.sendQueuedChanges))
+				completeProcessing()
 			}
 		}
 	}
@@ -250,6 +229,29 @@ extension CloudKitManager {
 		errorHandler?.presentError(error, title: "CloudKit Syncing Error")
 	}
 
+	private func writerRequests(_ requests: Set<CloudKitActionRequest>) {
+		let queuedRequests: Set<CloudKitActionRequest>
+		if let fileData = try? Data(contentsOf: CloudKitActionRequest.actionRequestFile) {
+			let decoder = PropertyListDecoder()
+			if let decodedRequests = try? decoder.decode(Set<CloudKitActionRequest>.self, from: fileData) {
+				queuedRequests = decodedRequests
+			} else {
+				queuedRequests = Set<CloudKitActionRequest>()
+			}
+		} else {
+			queuedRequests = Set<CloudKitActionRequest>()
+		}
+
+		let mergedRequests = queuedRequests.union(requests)
+		
+		let encoder = PropertyListEncoder()
+		encoder.outputFormat = .binary
+
+		if let encodedIDs = try? encoder.encode(mergedRequests) {
+			try? encodedIDs.write(to: CloudKitActionRequest.actionRequestFile)
+		}
+	}
+	
 	@objc private func sendQueuedChanges() {
 		guard isNetworkAvailable else {
 			return
@@ -265,7 +267,6 @@ extension CloudKitManager {
 		let completeProcessing = { [unowned self] in
 			self.modifications = [CKRecordZone.ID: ([CKRecord], [CKRecord.ID])]()
 			self.isSyncing = false
-			self.requestsFileLock.unlock()
 			
 			UIApplication.shared.endBackgroundTask(self.sendChangesBackgroundTaskID)
 			self.sendChangesBackgroundTaskID = UIBackgroundTaskIdentifier.invalid
@@ -273,30 +274,68 @@ extension CloudKitManager {
 			completion()
 		}
 
-		self.requestsFileLock.lock()
-
 		self.sendChangesBackgroundTaskID = UIApplication.shared.beginBackgroundTask {
 			completeProcessing()
 			os_log("CloudKit sync processing terminated for running too long.", log: self.log, type: .info)
 		}
 		
-		self.groupModificationAndSendToCloudKit() {
-			completeProcessing()
+		requestsFileQueue.async {
+			let combinedRequests = self.loadRequests()
+			DispatchQueue.main.async {
+				self.send(combinedRequests: combinedRequests) {
+					completeProcessing()
+				}
+			}
 		}
 	}
 	
-	private func groupModificationAndSendToCloudKit(completion: @escaping (() -> Void)) {
-		assert(Thread.isMainThread)
+	private func loadRequests() -> [String: CombinedRequest] {
+		var combinedRequests = [String: CombinedRequest]()
+
+		guard let queuedRequests = CloudKitActionRequest.loadRequests(), !queuedRequests.isEmpty else { return combinedRequests }
 		
-		guard let account = AccountManager.shared.cloudKitAccount,
-			  let cloudKitManager = account.cloudKitManager else {
-			completion()
-			return
+		for queuedRequest in queuedRequests {
+			switch queuedRequest.id {
+			case .document(_, let documentUUID):
+				if let combinedRequest = combinedRequests[documentUUID] {
+					combinedRequest.documentRequest = queuedRequest
+					combinedRequests[documentUUID] = combinedRequest
+				} else {
+					let combinedRequest = CombinedRequest()
+					combinedRequest.documentRequest = queuedRequest
+					combinedRequests[documentUUID] = combinedRequest
+				}
+			case .row(_, let documentUUID, _):
+				if let combinedRequest = combinedRequests[documentUUID] {
+					combinedRequest.rowRequests.append(queuedRequest)
+					combinedRequests[documentUUID] = combinedRequest
+				} else {
+					let combinedRequest = CombinedRequest()
+					combinedRequest.rowRequests.append(queuedRequest)
+					combinedRequests[documentUUID] = combinedRequest
+				}
+			case .image(_, let documentUUID, _, _):
+				if let combinedRequest = combinedRequests[documentUUID] {
+					combinedRequest.imageRequests.append(queuedRequest)
+					combinedRequests[documentUUID] = combinedRequest
+				} else {
+					let combinedRequest = CombinedRequest()
+					combinedRequest.imageRequests.append(queuedRequest)
+					combinedRequests[documentUUID] = combinedRequest
+				}
+			default:
+				fatalError()
+			}
 		}
 		
-		let combinedRequests = loadRequests()
-		
-		guard !combinedRequests.isEmpty else {
+		return combinedRequests
+	}
+	
+
+	private func send(combinedRequests: [String : CloudKitManager.CombinedRequest], completion: @escaping (() -> Void)) {
+		guard !combinedRequests.isEmpty,
+			  let account = AccountManager.shared.cloudKitAccount,
+			  let cloudKitManager = account.cloudKitManager else {
 			completion()
 			return
 		}
@@ -378,6 +417,119 @@ extension CloudKitManager {
 				self.deleteTempFiles(tempFileURLs)
 			}
 			completion()
+		}
+	}
+	
+	private func deleteRequests() {
+		try? FileManager.default.removeItem(at: CloudKitActionRequest.actionRequestFile)
+	}
+	
+	private func deleteTempFiles(_ urls: [URL]) {
+		for url in urls {
+			try? FileManager.default.removeItem(at: url)
+		}
+	}
+	
+	private func addSave(_ document: Document) {
+		guard let outline = document.outline, let zoneID = outline.zoneID else { return }
+		
+		outline.syncID = UUID().uuidString
+		
+		let recordID = CKRecord.ID(recordName: outline.id.description, zoneID: zoneID)
+		let record = CKRecord(recordType: CloudKitOutlineZone.CloudKitOutline.recordType, recordID: recordID)
+		
+		record[CloudKitOutlineZone.CloudKitOutline.Fields.syncID] = outline.syncID
+		record[CloudKitOutlineZone.CloudKitOutline.Fields.title] = outline.title
+		record[CloudKitOutlineZone.CloudKitOutline.Fields.ownerName] = outline.ownerName
+		record[CloudKitOutlineZone.CloudKitOutline.Fields.ownerEmail] = outline.ownerEmail
+		record[CloudKitOutlineZone.CloudKitOutline.Fields.ownerURL] = outline.ownerURL
+		record[CloudKitOutlineZone.CloudKitOutline.Fields.created] = outline.created
+		record[CloudKitOutlineZone.CloudKitOutline.Fields.updated] = outline.updated
+		record[CloudKitOutlineZone.CloudKitOutline.Fields.tagNames] = outline.tags.map { $0.name }
+		if let rowOrder = outline.rowOrder {
+			record[CloudKitOutlineZone.CloudKitOutline.Fields.rowOrder] = Array(rowOrder)
+		}
+		record[CloudKitOutlineZone.CloudKitOutline.Fields.documentLinks] = outline.documentLinks?.map { $0.description }
+		record[CloudKitOutlineZone.CloudKitOutline.Fields.documentBacklinks] = outline.documentBacklinks?.map { $0.description }
+		record[CloudKitOutlineZone.CloudKitOutline.Fields.hasAltLinks] = outline.hasAltLinks
+		record[CloudKitOutlineZone.CloudKitOutline.Fields.disambiguator] = outline.disambiguator
+
+		addSave(zoneID, record)
+	}
+	
+	private func addSave(zoneID: CKRecordZone.ID, outlineRecordID: CKRecord.ID, row: Row) {
+		row.syncID = UUID().uuidString
+		
+		let recordID = CKRecord.ID(recordName: row.entityID.description, zoneID: zoneID)
+		let record = CKRecord(recordType: CloudKitOutlineZone.CloudKitRow.recordType, recordID: recordID)
+		
+		record.parent = CKRecord.Reference(recordID: outlineRecordID, action: .none)
+		record[CloudKitOutlineZone.CloudKitRow.Fields.outline] = CKRecord.Reference(recordID: outlineRecordID, action: .deleteSelf)
+		record[CloudKitOutlineZone.CloudKitRow.Fields.syncID] = row.syncID
+		record[CloudKitOutlineZone.CloudKitRow.Fields.subtype] = "text"
+		record[CloudKitOutlineZone.CloudKitRow.Fields.topicData] = row.topicData
+		record[CloudKitOutlineZone.CloudKitRow.Fields.noteData] = row.noteData
+		record[CloudKitOutlineZone.CloudKitRow.Fields.isComplete] = row.isComplete ? "1" : "0"
+		record[CloudKitOutlineZone.CloudKitRow.Fields.rowOrder] = Array(row.rowOrder)
+
+		addSave(zoneID, record)
+	}
+	
+	private func addSave(zoneID: CKRecordZone.ID, image: Image) -> URL {
+		var image = image
+		
+		image.syncID = UUID().uuidString
+		
+		let recordID = CKRecord.ID(recordName: image.id.description, zoneID: zoneID)
+		let record = CKRecord(recordType: CloudKitOutlineZone.CloudKitImage.recordType, recordID: recordID)
+		
+		let rowID = EntityID.row(image.id.accountID, image.id.documentUUID, image.id.rowUUID)
+		let rowRecordID = CKRecord.ID(recordName: rowID.description, zoneID: zoneID)
+		
+		record.parent = CKRecord.Reference(recordID: rowRecordID, action: .none)
+		record[CloudKitOutlineZone.CloudKitImage.Fields.row] = CKRecord.Reference(recordID: rowRecordID, action: .deleteSelf)
+		record[CloudKitOutlineZone.CloudKitImage.Fields.isInNotes] = image.isInNotes
+		record[CloudKitOutlineZone.CloudKitImage.Fields.offset] = image.offset
+		record[CloudKitOutlineZone.CloudKitImage.Fields.syncID] = image.syncID
+
+		let imageURL = FileManager.default.temporaryDirectory.appendingPathComponent(image.id.imageUUID).appendingPathExtension("png")
+		try? image.data.write(to: imageURL)
+		record[CloudKitOutlineZone.CloudKitImage.Fields.asset] = CKAsset(fileURL: imageURL)
+
+		addSave(zoneID, record)
+		
+		return imageURL
+	}
+	
+	private func addSave(_ zoneID: CKRecordZone.ID, _ record: CKRecord) {
+		if let (saves, deletes) = modifications[zoneID] {
+			var mutableSaves = saves
+			mutableSaves.append(record)
+			modifications[zoneID] = (mutableSaves, deletes)
+		} else {
+			var saves = [CKRecord]()
+			saves.append(record)
+			let deletes = [CKRecord.ID]()
+			modifications[zoneID] = (saves, deletes)
+		}
+	}
+	
+	private func addDelete(_ request: CloudKitActionRequest) {
+		let zoneID = request.zoneID
+		let recordID = CKRecord.ID(recordName: request.id.description, zoneID: zoneID)
+		addDelete(zoneID, recordID)
+	}
+	
+	private func addDelete(_ zoneID: CKRecordZone.ID, _ recordID: CKRecord.ID) {
+		if let (saves, deletes) = modifications[zoneID] {
+			var mutableDeletes = deletes
+			mutableDeletes.append(recordID)
+			modifications[zoneID] = (saves, mutableDeletes)
+		} else {
+			let saves = [CKRecord]()
+			var deletes = [CKRecord.ID]()
+			deletes.append(recordID)
+			modifications[zoneID] = (saves, deletes)
 		}
 	}
 	
@@ -491,161 +643,6 @@ extension CloudKitManager {
 				return
 			}
 			UserDefaults.standard.set(data, forKey: sharedDatabaseChangeTokenKey)
-		}
-	}
-	
-	private func loadRequests() -> [String: CombinedRequest] {
-		var combinedRequests = [String: CombinedRequest]()
-
-		guard let queuedRequests = CloudKitActionRequest.loadRequests(), !queuedRequests.isEmpty else { return combinedRequests }
-		
-		for queuedRequest in queuedRequests {
-			switch queuedRequest.id {
-			case .document(_, let documentUUID):
-				if let combinedRequest = combinedRequests[documentUUID] {
-					combinedRequest.documentRequest = queuedRequest
-					combinedRequests[documentUUID] = combinedRequest
-				} else {
-					let combinedRequest = CombinedRequest()
-					combinedRequest.documentRequest = queuedRequest
-					combinedRequests[documentUUID] = combinedRequest
-				}
-			case .row(_, let documentUUID, _):
-				if let combinedRequest = combinedRequests[documentUUID] {
-					combinedRequest.rowRequests.append(queuedRequest)
-					combinedRequests[documentUUID] = combinedRequest
-				} else {
-					let combinedRequest = CombinedRequest()
-					combinedRequest.rowRequests.append(queuedRequest)
-					combinedRequests[documentUUID] = combinedRequest
-				}
-			case .image(_, let documentUUID, _, _):
-				if let combinedRequest = combinedRequests[documentUUID] {
-					combinedRequest.imageRequests.append(queuedRequest)
-					combinedRequests[documentUUID] = combinedRequest
-				} else {
-					let combinedRequest = CombinedRequest()
-					combinedRequest.imageRequests.append(queuedRequest)
-					combinedRequests[documentUUID] = combinedRequest
-				}
-			default:
-				fatalError()
-			}
-		}
-		
-		return combinedRequests
-	}
-	
-	private func deleteRequests() {
-		try? FileManager.default.removeItem(at: CloudKitActionRequest.actionRequestFile)
-	}
-	
-	private func deleteTempFiles(_ urls: [URL]) {
-		for url in urls {
-			try? FileManager.default.removeItem(at: url)
-		}
-	}
-	
-	private func addSave(_ document: Document) {
-		guard let outline = document.outline, let zoneID = outline.zoneID else { return }
-		
-		outline.syncID = UUID().uuidString
-		
-		let recordID = CKRecord.ID(recordName: outline.id.description, zoneID: zoneID)
-		let record = CKRecord(recordType: CloudKitOutlineZone.CloudKitOutline.recordType, recordID: recordID)
-		
-		record[CloudKitOutlineZone.CloudKitOutline.Fields.syncID] = outline.syncID
-		record[CloudKitOutlineZone.CloudKitOutline.Fields.title] = outline.title
-		record[CloudKitOutlineZone.CloudKitOutline.Fields.ownerName] = outline.ownerName
-		record[CloudKitOutlineZone.CloudKitOutline.Fields.ownerEmail] = outline.ownerEmail
-		record[CloudKitOutlineZone.CloudKitOutline.Fields.ownerURL] = outline.ownerURL
-		record[CloudKitOutlineZone.CloudKitOutline.Fields.created] = outline.created
-		record[CloudKitOutlineZone.CloudKitOutline.Fields.updated] = outline.updated
-		record[CloudKitOutlineZone.CloudKitOutline.Fields.tagNames] = outline.tags.map { $0.name }
-		if let rowOrder = outline.rowOrder {
-			record[CloudKitOutlineZone.CloudKitOutline.Fields.rowOrder] = Array(rowOrder)
-		}
-		record[CloudKitOutlineZone.CloudKitOutline.Fields.documentLinks] = outline.documentLinks?.map { $0.description }
-		record[CloudKitOutlineZone.CloudKitOutline.Fields.documentBacklinks] = outline.documentBacklinks?.map { $0.description }
-		record[CloudKitOutlineZone.CloudKitOutline.Fields.hasAltLinks] = outline.hasAltLinks
-		record[CloudKitOutlineZone.CloudKitOutline.Fields.disambiguator] = outline.disambiguator
-
-		addSave(zoneID, record)
-	}
-	
-	private func addSave(zoneID: CKRecordZone.ID, outlineRecordID: CKRecord.ID, row: Row) {
-		row.syncID = UUID().uuidString
-		
-		let recordID = CKRecord.ID(recordName: row.entityID.description, zoneID: zoneID)
-		let record = CKRecord(recordType: CloudKitOutlineZone.CloudKitRow.recordType, recordID: recordID)
-		
-		record.parent = CKRecord.Reference(recordID: outlineRecordID, action: .none)
-		record[CloudKitOutlineZone.CloudKitRow.Fields.outline] = CKRecord.Reference(recordID: outlineRecordID, action: .deleteSelf)
-		record[CloudKitOutlineZone.CloudKitRow.Fields.syncID] = row.syncID
-		record[CloudKitOutlineZone.CloudKitRow.Fields.subtype] = "text"
-		record[CloudKitOutlineZone.CloudKitRow.Fields.topicData] = row.topicData
-		record[CloudKitOutlineZone.CloudKitRow.Fields.noteData] = row.noteData
-		record[CloudKitOutlineZone.CloudKitRow.Fields.isComplete] = row.isComplete ? "1" : "0"
-		record[CloudKitOutlineZone.CloudKitRow.Fields.rowOrder] = Array(row.rowOrder)
-
-		addSave(zoneID, record)
-	}
-	
-	private func addSave(zoneID: CKRecordZone.ID, image: Image) -> URL {
-		var image = image
-		
-		image.syncID = UUID().uuidString
-		
-		let recordID = CKRecord.ID(recordName: image.id.description, zoneID: zoneID)
-		let record = CKRecord(recordType: CloudKitOutlineZone.CloudKitImage.recordType, recordID: recordID)
-		
-		let rowID = EntityID.row(image.id.accountID, image.id.documentUUID, image.id.rowUUID)
-		let rowRecordID = CKRecord.ID(recordName: rowID.description, zoneID: zoneID)
-		
-		record.parent = CKRecord.Reference(recordID: rowRecordID, action: .none)
-		record[CloudKitOutlineZone.CloudKitImage.Fields.row] = CKRecord.Reference(recordID: rowRecordID, action: .deleteSelf)
-		record[CloudKitOutlineZone.CloudKitImage.Fields.isInNotes] = image.isInNotes
-		record[CloudKitOutlineZone.CloudKitImage.Fields.offset] = image.offset
-		record[CloudKitOutlineZone.CloudKitImage.Fields.syncID] = image.syncID
-
-		let imageURL = FileManager.default.temporaryDirectory.appendingPathComponent(image.id.imageUUID).appendingPathExtension("png")
-		try? image.data.write(to: imageURL)
-		record[CloudKitOutlineZone.CloudKitImage.Fields.asset] = CKAsset(fileURL: imageURL)
-
-		addSave(zoneID, record)
-		
-		return imageURL
-	}
-	
-	private func addSave(_ zoneID: CKRecordZone.ID, _ record: CKRecord) {
-		if let (saves, deletes) = modifications[zoneID] {
-			var mutableSaves = saves
-			mutableSaves.append(record)
-			modifications[zoneID] = (mutableSaves, deletes)
-		} else {
-			var saves = [CKRecord]()
-			saves.append(record)
-			let deletes = [CKRecord.ID]()
-			modifications[zoneID] = (saves, deletes)
-		}
-	}
-	
-	private func addDelete(_ request: CloudKitActionRequest) {
-		let zoneID = request.zoneID
-		let recordID = CKRecord.ID(recordName: request.id.description, zoneID: zoneID)
-		addDelete(zoneID, recordID)
-	}
-	
-	private func addDelete(_ zoneID: CKRecordZone.ID, _ recordID: CKRecord.ID) {
-		if let (saves, deletes) = modifications[zoneID] {
-			var mutableDeletes = deletes
-			mutableDeletes.append(recordID)
-			modifications[zoneID] = (saves, mutableDeletes)
-		} else {
-			let saves = [CKRecord]()
-			var deletes = [CKRecord.ID]()
-			deletes.append(recordID)
-			modifications[zoneID] = (saves, deletes)
 		}
 	}
 	
