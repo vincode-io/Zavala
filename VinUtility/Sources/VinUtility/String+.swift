a72e1cb8f03158d2b10b36c4c96e2de8f26ed277
Maurice Parker <mo@vincode.io>
2023-11-28 17:05:58 -0600

    Merge String+ files

--- VinUtility/Sources/VinUtility/String+.swift
+++ VinUtility/Sources/VinUtility/String+.swift
@@ -2,13 +2,50 @@
 //  Created by Maurice Parker on 3/4/17.
 //  Copyright © 2017 Vincode. All rights reserved.
 //
+
+#if canImport(UIKit)
+import UIKit
+#else
 import Foundation
+#endif
 
 public extension String {
 	
 	init(localized: String, comment: String? = nil) {
 		self = localized
 	}
+	
+	var containsSymbols: Bool {
+		for scalar in unicodeScalars {
+			switch scalar.value {
+			case 0x1F600...0x1F64F, // Emoticons
+				 0x1F300...0x1F5FF, // Misc Symbols and Pictographs
+				 0x1F680...0x1F6FF, // Transport and Map
+				 0x2300...0x23FF,   // Misc Technical symbols
+				 0x2600...0x26FF,   // Misc symbols
+				 0x2700...0x27BF,   // Dingbats
+				 0xFE00...0xFE0F:   // Variation Selectors
+				return true
+			default:
+				continue
+			}
+		}
+		return false
+	}
+	
+	#if canImport(UIKit)
+	func height(withConstrainedWidth width: CGFloat, font: UIFont) -> CGFloat {
+		let constraintRect = CGSize(width: width, height: .greatestFiniteMagnitude)
+		let boundingBox = self.boundingRect(with: constraintRect, options: [.usesLineFragmentOrigin, .usesFontLeading], attributes: [NSAttributedString.Key.font: font], context: nil)
+		return ceil(boundingBox.height)
+	}
+	
+	func width(withConstrainedHeight height: CGFloat, font: UIFont) -> CGFloat {
+		let constraintRect = CGSize(width: .greatestFiniteMagnitude, height: height)
+		let boundingBox = self.boundingRect(with: constraintRect, options: [.usesLineFragmentOrigin, .usesFontLeading], attributes: [NSAttributedString.Key.font: font], context: nil)
+		return ceil(boundingBox.width)
+	}
+	#endif
 	
 	var queryEncoded: String? {
 		return addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)
