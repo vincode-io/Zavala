d73e00e4fcaa6ce5a4092a9e9472717ff0d5eb31
Maurice Parker <mo@vincode.io>
2024-01-29 18:49:15 -0600

    Fix how we are using the recordChangeTag

--- VinOutlineKit/Sources/VinOutlineKit/Image+CloudKit.swift
+++ VinOutlineKit/Sources/VinOutlineKit/Image+CloudKit.swift
@@ -27,9 +27,11 @@ extension Image: VCKModel {
     }
     
     public func apply(_ record: CKRecord) -> Bool {
-		guard let metaData = cloudKitMetaData,
-			  let recordChangeTag = CKRecord(metaData)?.recordChangeTag,
-			  record.recordChangeTag != recordChangeTag else { return false }
+		if let metaData = cloudKitMetaData,
+		   let recordChangeTag = CKRecord(metaData)?.recordChangeTag,
+		   record.recordChangeTag == recordChangeTag {
+			return false
+		}
 
 		cloudKitMetaData = record.metadata
 
