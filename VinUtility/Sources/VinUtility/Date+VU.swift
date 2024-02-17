66ab62e975f2545213fbfdbfd88aae2869b341e6
Maurice Parker <mo@vincode.io>
2024-02-17 14:04:03 -0600

    Make thread-safe

--- VinUtility/Sources/VinUtility/Date+VU.swift
+++ VinUtility/Sources/VinUtility/Date+VU.swift
@@ -5,50 +5,37 @@
 
 import Foundation
 
-private let rfc822dateFormatter: DateFormatter = {
-	let dateFormatter = DateFormatter()
-	dateFormatter.locale = Locale(identifier: "en_US_POSIX")
-	dateFormatter.timeZone = TimeZone(secondsFromGMT: 0)
-
-	return dateFormatter
-}()
-
-private let rfc822dateFormatsWithComma = ["EEE, d MMM yyyy HH:mm:ss zzz", "EEE, d MMM yyyy HH:mm zzz", "EEE, d MMM yyyy HH:mm:ss", "EEE, d MMM yyyy HH:mm"]
-private let rfc822dateFormatsWithoutComma = ["d MMM yyyy HH:mm:ss zzz", "d MMM yyyy HH:mm zzz", "d MMM yyyy HH:mm:ss", "d MMM yyyy HH:mm"]
-
-private var rfc822LastUsedDateFormat: String?
-
 public extension Date {
 	
 	var rfc822String: String? {
-		if rfc822LastUsedDateFormat != rfc822dateFormatsWithComma[0] {
-			rfc822dateFormatter.dateFormat = rfc822dateFormatsWithComma[0]
-		}
-		return rfc822dateFormatter.string(from: self)
+		let dateFormatter = DateFormatter()
+		dateFormatter.locale = Locale(identifier: "en_US_POSIX")
+		dateFormatter.timeZone = TimeZone(secondsFromGMT: 0)
+		dateFormatter.dateFormat = "EEE, d MMM yyyy HH:mm:ss zzz"
+		return dateFormatter.string(from: self)
 	}
 	
 	static func dateFromRFC822(rfc822String: String) -> Date? {
-		let rfc822String = rfc822String.uppercased()
+		let dateFormatter = DateFormatter()
+		dateFormatter.locale = Locale(identifier: "en_US_POSIX")
+		dateFormatter.timeZone = TimeZone(secondsFromGMT: 0)
 
-		if rfc822LastUsedDateFormat != nil {
-			if let date = rfc822dateFormatter.date(from: rfc822String) {
-				return date
-			}
-		}
+		let rfc822dateFormatsWithComma = ["EEE, d MMM yyyy HH:mm:ss zzz", "EEE, d MMM yyyy HH:mm zzz", "EEE, d MMM yyyy HH:mm:ss", "EEE, d MMM yyyy HH:mm"]
+		let rfc822dateFormatsWithoutComma = ["d MMM yyyy HH:mm:ss zzz", "d MMM yyyy HH:mm zzz", "d MMM yyyy HH:mm:ss", "d MMM yyyy HH:mm"]
+
+		let rfc822String = rfc822String.uppercased()
 
 		if rfc822String.contains(",") {
 			for dateFormat in rfc822dateFormatsWithComma {
-				rfc822dateFormatter.dateFormat = dateFormat
-				if let date = rfc822dateFormatter.date(from: rfc822String) {
-					rfc822LastUsedDateFormat = dateFormat
+				dateFormatter.dateFormat = dateFormat
+				if let date = dateFormatter.date(from: rfc822String) {
 					return date
 				}
 			}
 		} else {
 			for dateFormat in rfc822dateFormatsWithoutComma {
-				rfc822dateFormatter.dateFormat = dateFormat
-				if let date = rfc822dateFormatter.date(from: rfc822String) {
-					rfc822LastUsedDateFormat = dateFormat
+				dateFormatter.dateFormat = dateFormat
+				if let date = dateFormatter.date(from: rfc822String) {
 					return date
 				}
 			}
