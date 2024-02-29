2d977b70895de4201cac355754246744e5cf5f44
Maurice Parker <mo@vincode.io>
2024-02-29 11:20:40 -0600

    Move multiple Outlines when multiple are selected and moving between accounts

--- Zavala/Collections/CollectionsViewController+Drop.swift
+++ Zavala/Collections/CollectionsViewController+Drop.swift
@@ -44,14 +44,13 @@ extension CollectionsViewController: UICollectionViewDropDelegate {
 	}
 	
 	func collectionView(_ collectionView: UICollectionView, performDropWith coordinator: UICollectionViewDropCoordinator) {
-		guard let dragItem = coordinator.items.first?.dragItem,
-			  let destinationIndexPath = coordinator.destinationIndexPath,
+		guard let destinationIndexPath = coordinator.destinationIndexPath,
 			  let item = dataSource.itemIdentifier(for: destinationIndexPath),
 			  let entityID = item.entityID,
 			  let container = AccountManager.shared.findDocumentContainer(entityID) else { return }
 		
 		// Dragging an OPML file into the Collections View
-		guard let document = dragItem.localObject as? Document else {
+		guard coordinator.items.first?.dragItem.localObject != nil else {
 			for dropItem in coordinator.items {
 				let provider = dropItem.dragItem.itemProvider
 				provider.loadDataRepresentation(forTypeIdentifier: DataRepresentation.opml.typeIdentifier) { (opmlData, error) in
@@ -70,42 +69,46 @@ extension CollectionsViewController: UICollectionViewDropDelegate {
 			return
 		}
 
-		// Local copy between accounts
-		guard document.account == container.account else {
-			document.load()
-			
-			var tagNames = [String]()
-			for tag in document.tags ?? [Tag]() {
-				document.deleteTag(tag)
-				document.account?.deleteTag(tag)
-				tagNames.append(tag.name)
-			}
-			
-			document.account?.deleteDocument(document)
-			if let containerAccount = container.account {
-				document.reassignAccount(containerAccount.id.accountID)
-				containerAccount.createDocument(document)
-			}
-			
-			for tagName in tagNames {
-				if let tag = container.account?.createTag(name: tagName) {
+		for dropItem in coordinator.items {
+			if let document = dropItem.dragItem.localObject as? Document {
+				guard document.account != container.account else {
+					guard let tag = (container as? TagDocuments)?.tag else {
+						return
+					}
+
+					// Adding a tag by dragging a document to it within the same account
 					document.createTag(tag)
+					return
 				}
+				
+				// Local copy between accounts
+				document.load()
+				
+				var tagNames = [String]()
+				for tag in document.tags ?? [Tag]() {
+					document.deleteTag(tag)
+					document.account?.deleteTag(tag)
+					tagNames.append(tag.name)
+				}
+				
+				document.account?.deleteDocument(document)
+				if let containerAccount = container.account {
+					document.reassignAccount(containerAccount.id.accountID)
+					containerAccount.createDocument(document)
+				}
+				
+				for tagName in tagNames {
+					if let tag = container.account?.createTag(name: tagName) {
+						document.createTag(tag)
+					}
+				}
+				
+				document.deleteAllBacklinks()
+				
+				document.forceSave()
+				document.unload()
 			}
-			
-			document.deleteAllBacklinks()
-			
-			document.forceSave()
-			document.unload()
-			return
 		}
-		
-		// Adding a tag by dragging a document to it
-		guard let tag = (container as? TagDocuments)?.tag else {
-			return
-		}
-		
-		document.createTag(tag)
 	}
 	
 	
