78c5bc74a9594cccd797a723e9f76eb927fc9a0d
Maurice Parker <mo@vincode.io>
2024-04-06 17:39:47 -0500

    Throw an appropriate error when CloudKit per row limits are exceeded

--- VinCloudKit/Sources/VinCloudKit/VCKError.swift
+++ VinCloudKit/Sources/VinCloudKit/VCKError.swift
@@ -10,6 +10,7 @@ import Foundation
 public enum VCKError: LocalizedError {
 	case userDeletedZone
 	case corruptAccount
+	case maxChildCountExceeded
 	case unknown
 	
 	public var errorDescription: String? {
@@ -20,6 +21,9 @@ public enum VCKError: LocalizedError {
 		case .corruptAccount:
 			return String(localized: "There is an unrecoverable problem with your application iCloud account. Please make sure you have iCloud and iCloud Drive enabled in System Preferences. Then remove the application iCloud account and add it again.", 
 									 comment: "Error Message: Corrupt account.")
+		case .maxChildCountExceeded:
+			return String(localized: "The maximum number of child rows that iCloud can sync is 750. This limit has been exceeded. Please reduce the number of child rows to continue syncing.",
+									 comment: "Error Message: Max child count exceeeded.")
 		default:
 			return String(localized: "An unexpected CloudKit error occurred.", 
 									 comment: "Error Message: An unexpected CloudKit error occurred.")
