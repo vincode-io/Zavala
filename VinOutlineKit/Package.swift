9e0616881b6ffb4f8ababfff6fef4fb9d0fa507b
Maurice Parker <mo@vincode.io>
2024-06-29 12:43:38 -0500

    Change all packages to use Swift 6

--- VinOutlineKit/Package.swift
+++ VinOutlineKit/Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.9
+// swift-tools-version: 6.0
 import PackageDescription
 
 let package = Package(
@@ -26,8 +26,7 @@ let package = Package(
 				"Semaphore",
 				"VinXML",
 				"VinCloudKit",
-			],
-			swiftSettings: [.enableExperimentalFeature("StrictConcurrency")]
+			]
 		),
 		.testTarget(
 			name: "VinOutlineKitTests",
--- VinUtility/Package.swift
+++ VinUtility/Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.9
+// swift-tools-version: 6.0
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -22,8 +22,7 @@ let package = Package(
 		dependencies: [
 			.product(name: "OrderedCollections", package: "swift-collections"),
 			.product(name: "AsyncAlgorithms", package: "swift-async-algorithms"),
-		],
-		swiftSettings: [.enableExperimentalFeature("StrictConcurrency")]
+		]
 	),
 	]
 )
