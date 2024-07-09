648d74d16bbaed14ad2c98c07c17b37ba6c0c36a
Maurice Parker <mo@vincode.io>
2024-07-09 15:45:44 -0500

    Migrate code to App Intents

--- Zavala/AppIntents/Outline/RemoveOutlineTagAppIntent.swift
+++ Zavala/AppIntents/Outline/RemoveOutlineTagAppIntent.swift
@@ -8,16 +8,16 @@
 import Foundation
 import AppIntents
 
-struct RemoveOutlineTagAppIntent: AppIntent, CustomIntentMigratedAppIntent, PredictableIntent {
+struct RemoveOutlineTagAppIntent: AppIntent, CustomIntentMigratedAppIntent, PredictableIntent, ZavalaAppIntent {
     static let intentClassName = "RemoveOutlineTagIntent"
     static let title: LocalizedStringResource = "Remove Outline Tag"
     static let description = IntentDescription("Removes a Tag from the given Outline.")
 
     @Parameter(title: "Outline")
-	var outline: OutlineAppEntity?
+	var outline: OutlineAppEntity
 
     @Parameter(title: "Tag Name")
-    var tagName: String?
+    var tagName: String
 
     static var parameterSummary: some ParameterSummary {
         Summary("Remove \(\.$tagName) from \(\.$outline)")
@@ -26,21 +26,27 @@ struct RemoveOutlineTagAppIntent: AppIntent, CustomIntentMigratedAppIntent, Pred
     static var predictionConfiguration: some IntentPredictionConfiguration {
         IntentPrediction(parameters: (\.$outline, \.$tagName)) { outline, tagName in
             DisplayRepresentation(
-                title: "Remove \(tagName!) from \(outline!)",
+                title: "Remove \(tagName) from \(outline)",
                 subtitle: ""
             )
         }
     }
 
     func perform() async throws -> some IntentResult {
-        return .result()
+		await resume()
+		
+		guard let outline = await findOutline(outline) else {
+			await suspend()
+			throw ZavalaAppIntentError.outlineNotFound
+		}
+		
+		if let tag = await outline.account?.findTag(name: tagName) {
+			await outline.deleteTag(tag)
+			await outline.account?.deleteTag(tag)
+		}
+
+		await suspend()
+		return .result()
     }
 	
 }
-
-private extension IntentDialog {
-    static func tagNameParameterPrompt(tagName: String) -> Self {
-        "What is the \(tagName) to remove?"
-    }
-}
-
