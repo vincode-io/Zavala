dd8abd66d9aae843ba721fa744347ad9176465fa
Maurice Parker <mo@vincode.io>
2024-07-09 15:12:42 -0500

    Migrate to the new App Intents framework

--- Zavala/AppIntents/Outline/ImportAppIntent.swift
+++ Zavala/AppIntents/Outline/ImportAppIntent.swift
@@ -7,23 +7,24 @@
 
 import Foundation
 import AppIntents
+import VinOutlineKit
 
-struct ImportAppIntent: AppIntent, CustomIntentMigratedAppIntent, PredictableIntent {
+struct ImportAppIntent: AppIntent, CustomIntentMigratedAppIntent, PredictableIntent, ZavalaAppIntent {
     static let intentClassName = "ImportIntent"
     static let title: LocalizedStringResource = "Import"
     static let description = IntentDescription("Import an outline.")
 
     @Parameter(title: "Input File")
-    var inputFile: IntentFile?
+    var inputFile: IntentFile
 
     @Parameter(title: "Input Images")
     var inputImages: [IntentFile]?
 
     @Parameter(title: "Import Type")
-	var importType: ImportTypeAppEnum?
+	var importType: ImportTypeAppEnum
 
     @Parameter(title: "Account Type")
-	var accountType: AccountTypeAppEnum?
+	var accountType: AccountTypeAppEnum
 
     static var parameterSummary: some ParameterSummary {
         Summary("Import the \(\.$importType) \(\.$inputFile) into \(\.$accountType)") {
@@ -34,39 +35,37 @@ struct ImportAppIntent: AppIntent, CustomIntentMigratedAppIntent, PredictableInt
     static var predictionConfiguration: some IntentPredictionConfiguration {
         IntentPrediction(parameters: (\.$inputFile, \.$importType, \.$accountType, \.$inputImages)) { inputFile, importType, accountType, inputImages in
             DisplayRepresentation(
-                title: "Import the\(importType!)\(inputFile!)into\(accountType!)",
+                title: "Import the \(importType) \(inputFile) into \(accountType)",
                 subtitle: ""
             )
         }
     }
 
 	func perform() async throws -> some IntentResult & ReturnsValue<OutlineAppEntity> {
-        // TODO: Place your refactored intent handler code here.
-        return .result(value: OutlineAppEntity(/* fill in result initializer here */))
-    }
-}
+        await resume()
+		
+		let acctType = accountType == .onMyDevice ? AccountType.local : AccountType.cloudKit
+		guard let account = await AccountManager.shared.findAccount(accountType: acctType) else {
+			await suspend()
+			throw ZavalaAppIntentError.unavailableAccount
+		}
+		
+		var images = [String:  Data]()
+		if let inputImages {
+			for intentImage in inputImages {
+				let imageUUID = String(intentImage.filename.prefix(while: { $0 != "." }))
+				images[imageUUID] = intentImage.data
+			}
+		}
+		
+		guard let outline = try? await account.importOPML(inputFile.data, tags: nil, images: images).outline else {
+			await suspend()
+			throw ZavalaAppIntentError.unexpectedError
+		}
 
-private extension IntentDialog {
-    static var inputFileParameterPrompt: Self {
-        "Please supply the Input File."
-    }
-	static func importTypeParameterDisambiguationIntro(count: Int, importType: ImportTypeAppEnum) -> Self {
-        "There are \(count) options matching ‘\(importType)’."
-    }
-	static func importTypeParameterConfirmation(importType: ImportTypeAppEnum) -> Self {
-        "Just to confirm, you wanted ‘\(importType)’?"
-    }
-    static var importTypeParameterRequired: Self {
-        "The Import Type is required."
-    }
-	static func accountTypeParameterDisambiguationIntro(count: Int, accountType: AccountTypeAppEnum) -> Self {
-        "There are \(count) options matching ‘\(accountType)’."
-    }
-	static func accountTypeParameterConfirmation(accountType: AccountTypeAppEnum) -> Self {
-        "Just to confirm, you wanted ‘\(accountType)’?"
-    }
-    static var accountTypeParameterRequired: Self {
-        "The Account Type is required."
+		
+		await suspend()
+		return await .result(value: OutlineAppEntity(outline: outline))
     }
 }
 
