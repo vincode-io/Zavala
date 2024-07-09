76daaccbdc2808c67354139be9a93ea7bdf6151c
Maurice Parker <mo@vincode.io>
2024-07-09 15:32:34 -0500

    Migrate code to App Intents

--- Zavala/AppIntents/Outline/RemoveOutlineAppIntent.swift
+++ Zavala/AppIntents/Outline/RemoveOutlineAppIntent.swift
@@ -8,31 +8,41 @@
 import Foundation
 import AppIntents
 
-struct RemoveOutlineAppIntent: AppIntent, CustomIntentMigratedAppIntent, PredictableIntent {
+struct RemoveOutlineAppIntent: AppIntent, CustomIntentMigratedAppIntent, PredictableIntent, ZavalaAppIntent {
     static let intentClassName = "RemoveOutlineIntent"
     static let title: LocalizedStringResource = "Remove Outline"
     static let description = IntentDescription("Deletes an Outline.")
 
     @Parameter(title: "Outline")
-	var outline: OutlineAppEntity?
+	var outline: OutlineAppEntity
 
     static var parameterSummary: some ParameterSummary {
-        Summary("Remove\(\.$outline)")
+        Summary("Remove \(\.$outline)")
     }
 
     static var predictionConfiguration: some IntentPredictionConfiguration {
         IntentPrediction(parameters: (\.$outline)) { outline in
             DisplayRepresentation(
-                title: "Remove \(outline!)",
+                title: "Remove \(outline)",
                 subtitle: ""
             )
         }
     }
 
     func perform() async throws -> some IntentResult {
-        // TODO: Place your refactored intent handler code here.
-        return .result()
+		await resume()
+		
+		guard let outline = await findOutline(outline) else {
+			await suspend()
+			throw ZavalaAppIntentError.outlineNotFound
+		}
+		
+		await outline.account?.deleteDocument(.outline(outline))
+		
+		await suspend()
+		return .result()
     }
+	
 }
 
 
