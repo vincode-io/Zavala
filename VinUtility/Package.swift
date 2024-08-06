095550823f6ee677ac7f634055e73eb3bf541934
Maurice Parker <mo@vincode.io>
2024-08-06 16:46:03 -0500

    Upgrade async algorithms

--- VinUtility/Package.swift
+++ VinUtility/Package.swift
@@ -14,7 +14,7 @@ let package = Package(
     ],
 	dependencies: [
 		.package(url: "https://github.com/apple/swift-collections.git", .upToNextMajor(from: "1.0.2")),
-		.package(url: "https://github.com/apple/swift-async-algorithms", from: "1.0.0"),
+		.package(url: "https://github.com/apple/swift-async-algorithms", .upToNextMajor(from: "1.0.0")),
 	],
 	targets: [
         .target(
