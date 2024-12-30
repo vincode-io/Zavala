c1e59f93dec3c6e1d425f02a60c75b59c80f42f1
Maurice Parker <mo@vincode.io>
2024-12-30 17:38:59 -0600

    Expand the current row if it is collapsed adn we are creating a row inside.

--- VinOutlineKit/Sources/VinOutlineKit/Commands/CreateRowInsideCommand.swift
+++ VinOutlineKit/Sources/VinOutlineKit/Commands/CreateRowInsideCommand.swift
@@ -11,6 +11,8 @@ public final class CreateRowInsideCommand: OutlineCommand {
 
 	var row: Row?
 	var afterRow: Row
+	var expandedRow: Row?
+	
 	var rowStrings: RowStrings?
 	
 	public init(actionName: String, undoManager: UndoManager, delegate: OutlineCommandDelegate, outline: Outline, afterRow: Row, rowStrings: RowStrings?) {
@@ -24,12 +26,21 @@ public final class CreateRowInsideCommand: OutlineCommand {
 		if row == nil {
 			row = Row(outline: outline)
 		}
+		
+		if afterRow.isExpandable {
+			expandedRow = outline.expand(rows: [afterRow]).first
+		}
+		
 		outline.createRowsInsideAtStart([row!], afterRowContainer: afterRow, rowStrings: rowStrings)
 	}
 	
 	public override func undo() {
 		guard let row else { return }
 		outline.deleteRows([row])
+		
+		if let expandedRow {
+			outline.collapse(rows: [expandedRow])
+		}
 	}
 	
 }
