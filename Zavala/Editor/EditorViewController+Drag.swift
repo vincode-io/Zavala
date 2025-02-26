cd98730d18df1f325b4140d7b7d3ce50ac66b5ca
Maurice Parker <mo@vincode.io>
2025-02-26 16:02:55 -0600

    Correct how we were doing Drag operations

--- Zavala/Editor/EditorViewController+Drag.swift
+++ Zavala/Editor/EditorViewController+Drag.swift
@@ -14,49 +14,40 @@ extension EditorViewController: UICollectionViewDragDelegate {
 	func collectionView(_ collectionView: UICollectionView, itemsForBeginning session: UIDragSession, at indexPath: IndexPath) -> [UIDragItem] {
 		guard indexPath.section == adjustedRowsSection, let shadowTable = outline?.shadowTable else { return [] }
 		
-		var dragItems = [UIDragItem]()
+		let row = shadowTable[indexPath.row]
 		
-		let indicatedRow = shadowTable[indexPath.row]
-		let rows = currentRows?.sortedWithDecendentsFiltered() ?? [indicatedRow]
-
-		guard indicatedRow == rows.first else { return [] }
-		
-		for row in rows {
-			let itemProvider = NSItemProvider()
-
-			// We only register the text representation on the first one, since it looks like most text editors only support 1 dragged text item
-			if row == rows[0] {
-				var markdowns = [String]()
-				for row in rows {
-					markdowns.append(row.markdownList(numberingStyle: .none))
-				}
-				let markdownData = markdowns.joined(separator: "\n").data(using: .utf8)
-
-				itemProvider.registerDataRepresentation(forTypeIdentifier: UTType.utf8PlainText.identifier, visibility: .all) { completion in
-					completion(markdownData, nil)
-					return nil
-				}
+		for currentRow in currentRows ?? [] {
+			if row.isDecendent(currentRow) {
+				return []
 			}
-			
-			if let rowData = try? RowGroup(row).asData() {
-				itemProvider.registerDataRepresentation(forTypeIdentifier: Row.typeIdentifier, visibility: .ownProcess) { completion in
-					completion(rowData, nil)
-					return nil
-				}
-			}
-			
-			let dragItem = UIDragItem(itemProvider: itemProvider)
-			dragItem.localObject = row
-		
-			dragItem.previewProvider = { () -> UIDragPreview? in
-				guard let cell = collectionView.cellForItem(at: indexPath) as? EditorRowViewCell else { return nil}
-				return UIDragPreview(view: cell, parameters: EditorRowPreviewParameters(cell: cell))
-			}
-			
-			dragItems.append(dragItem)
 		}
 		
-		return dragItems
+		let itemProvider = NSItemProvider()
+
+		let markdownData = row.markdownList(numberingStyle: .none).data(using: .utf8)
+		itemProvider.registerDataRepresentation(forTypeIdentifier: UTType.utf8PlainText.identifier, visibility: .all) { completion in
+			completion(markdownData, nil)
+			return nil
+		}
+		
+		if let rowData = try? RowGroup(row).asData() {
+			itemProvider.registerDataRepresentation(forTypeIdentifier: Row.typeIdentifier, visibility: .ownProcess) { completion in
+				completion(rowData, nil)
+				return nil
+			}
+		}
+		
+		let dragItem = UIDragItem(itemProvider: itemProvider)
+		dragItem.localObject = row
+	
+		dragItem.previewProvider = { () -> UIDragPreview? in
+			guard let shadowTableIndex = row.shadowTableIndex else { return nil }
+			let indexPath = IndexPath(row: shadowTableIndex, section: indexPath.section)
+			guard let cell = collectionView.cellForItem(at: indexPath) as? EditorRowViewCell else { return nil}
+			return UIDragPreview(view: cell, parameters: EditorRowPreviewParameters(cell: cell))
+		}
+		
+		return [dragItem]
 	}
 	
 }
