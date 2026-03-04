94646ac4b8798890470e77d7ab8e2223b6d119f1
Maurice Parker <mo@vincode.io>
2026-03-03 19:54:30 -0600

    Make the URL non-optional

--- Zavala/ShortcutRunner.swift
+++ Zavala/ShortcutRunner.swift
@@ -8,7 +8,7 @@
 struct ShortcutRunner {
 
 	private let defaultShortcutName = "Create an Outline with AI"
-	private let defaultShortcutURL = URL(string: "https://zavala.vincode.io/assets/shortcuts/Create_an_Outline_with_AI.shortcut")
+	private let defaultShortcutURL = URL(string: "https://zavala.vincode.io/assets/shortcuts/Create_an_Outline_with_AI.shortcut")!
 
 	init () {
 		let initialized = AppDefaults.shared.initialShortcutsMenuSetup
