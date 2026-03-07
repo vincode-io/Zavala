be96a7b1f538ac243a674cb1053690c2b3261e22
Maurice Parker <mo@vincode.io>
2026-03-07 08:09:06 -0600

    Handle handle topic and note parsing and formatting

--- VinOutlineKit/Sources/VinOutlineKit/MarkdownParser.swift
+++ VinOutlineKit/Sources/VinOutlineKit/MarkdownParser.swift
@@ -86,18 +86,62 @@ public struct MarkdownParser: MarkupWalker {
 		}
 
 		var topic = String()
-		
+		var note = String()
+
+		// Split up and format the Row strings
 		for i in 0..<listItem.childCount {
+
 			if let paragraph = listItem.child(at: i) as? Paragraph {
-				if i > 0 {
-					topic.append("\n\n")
+
+				// The first paragraph is the Topic. Everything else is Notes. Split
+				// the lines and trim them so that they don't have a bunch of extraneous
+				// leading whitespace.
+				if i == 0 {
+
+					let lines = paragraph.format().split(separator: "\n")
+
+					for j in 0..<lines.count {
+						let topicLine = String(lines[j]).trimmed()
+
+						topic.append(topicLine ?? "")
+
+						// Don't give the last topic line a newline
+						if j < lines.count - 1 {
+							note.append("\n")
+						}
+					}
+
+				} else {
+
+					// After the first note paragraph, start separating them with newlines
+					if i > 1 {
+						note.append("\n\n")
+					}
+
+					let lines = paragraph.format().split(separator: "\n")
+
+					for j in 0..<lines.count {
+						let noteLine = String(lines[j]).trimmed()
+
+						// It looks like the first line in a notes paragraph is
+						// blank.
+						if noteLine == nil && j == 0 { continue }
+
+						note.append(noteLine ?? "")
+
+						// Don't give the last note line a newline
+						if j < lines.count - 1 {
+							note.append("\n")
+						}
+					}
 				}
-				topic.append(paragraph.format().trimmed() ?? "")
+
 			}
+
 		}
 
 		MainActor.assumeIsolated {
-			let row = Row(outline: outline, topicMarkdown: topic)
+			let row = Row(outline: outline, topicMarkdown: topic, noteMarkdown: note)
 			row.detectData()
 
 			if let parentRow = parentRowStack.last {
