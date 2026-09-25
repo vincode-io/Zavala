378faeeeee881863756ac3ae5f7613815e1410c0
Maurice Parker <mo@vincode.io>
2026-09-24 19:10:19 -0500

    Revert "Made OutlineAppEntity indexable"
    This reverts commit 2ddb87532a30dc238a2b97592c164fa298cdb8da.

--- Zavala/AppIntents/OutlineAppEntity.swift
+++ Zavala/AppIntents/OutlineAppEntity.swift
@@ -10,7 +10,7 @@ import CoreTransferable
 import AppIntents
 import VinOutlineKit
 
-struct OutlineAppEntity: AppEntity, IndexedEntity, Equatable {
+struct OutlineAppEntity: AppEntity, Equatable {
 	static let typeDisplayRepresentation = TypeDisplayRepresentation(
 		name: LocalizedStringResource("label.text.outline-name", defaultValue: "Outline", table: "AppIntents", comment: "The name of the Outline type. Singular/plural forms live in AppIntents.stringsdict; Shortcuts uses the plural for 'Find Outlines'."),
 		numericFormat: LocalizedStringResource("label.text.outline-count", defaultValue: "\(placeholder: .int) Outlines", comment: "A count of Outlines, e.g. '2 Outlines'")
@@ -50,9 +50,6 @@ struct OutlineAppEntity: AppEntity, IndexedEntity, Equatable {
 	@Property(title: LocalizedStringResource("label.text.account-type", comment: "Account Type"))
 	var accountType: AccountTypeAppEnum?
 
-	@Property(title: LocalizedStringResource("label.text.list-print", comment: "List Print"), indexingKey: \.textContent)
-	var listPrint: AttributedString
-
     var displayRepresentation: DisplayRepresentation {
 		DisplayRepresentation(stringLiteral: title ?? .noTitleLabel)
     }
@@ -82,8 +79,6 @@ struct OutlineAppEntity: AppEntity, IndexedEntity, Equatable {
 		} else {
 			self.accountType = .onMyDevice
 		}
-
-		self.listPrint = AttributedString(outline.printList())
 	}
 	
 }
